eb10494cf3bb14a889562bf479a33afb1920349e
Sergey Borovkov <serge.borovkov@gmail.com>
2024-07-05 10:34:49 +0400

    Disable blam by defauly it's slow

--- lua/plugins/blam.lua
+++ lua/plugins/blam.lua
@@ -3,7 +3,11 @@ return {
   build = "make",
   opts = {
     -- options
-    enabled = true,
+    enabled = false,
   },
+
+  init = function()
+    vim.keymap.set("n", "<leader>ba", require("blam").peek)
+  end,
   config = true,
 }
