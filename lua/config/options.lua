d104ed8ca7fe9245d9ce587edbdd07a7023e4aa5
Sergey Borovkov <serge.borovkov@gmail.com>
2024-07-10 01:36:33 +0400

    Updates

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -4,7 +4,8 @@
 -- Set to "basedpyright" to use basedpyright instead of pyright.
 vim.g.python3_host_prog = "/usr/bin/python3"
 vim.g.lazyvim_python_lsp = "basedpyright"
-vim.opt.relativenumber = true
 
 local opt = vim.opt
 opt.grepprg = "rg --vimgrep --smart-case"
+opt.number = true
+opt.relativenumber = true
---
+++ lua/plugins/nightfox.lua
@@ -0,0 +1,6 @@
+return {
+  { "EdenEast/nightfox.nvim" },
+  { "LazyVim/Lazyvim", opts = {
+    colorscheme = "carbonfox",
+  } },
+}
