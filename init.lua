6d8bea4d88e8a1793fffec8be17b299b3b386783
Sergey Borovkov <serge.borovkov@gmail.com>
2026-01-05 10:35:52 +0400

    asdf

--- init.lua
+++ init.lua
@@ -1,12 +1,2 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-
-vim.opt.relativenumber = true
-
-vim.g.cmp_widths = {
-  abbr = 250,
-  menu = 100,
-}
-
-vim.opt.spell = false
-vim.opt.clipboard = "unnamedplus"
--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,12 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local map = LazyVim.safe_keymap_set
+
+-- Copy full path of current file
+map("n", "<leader>fP", function()
+  local path = vim.fn.expand("%:p")
+  vim.fn.setreg("+", path)
+  vim.notify("Copied: " .. path, vim.log.levels.INFO)
+end, { desc = "Copy full file path" })
